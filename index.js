22f5c551547fd29488a2b330814e778c60b43647
Lydia Bell <lrbell84@gmail.com>
2021-07-10 22:00:51 +0000

    commit tree 91c11d9d598b09ce8b0ce5d9db46d11a9baaeb16

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(event = "roller-skate") {
+    return `This Saturday, I want to ${event}!`;
+}// Your code here
+
+
+let mondayWork = function(event = "go to the office") {
+    return `This Monday, I will ${event}.`
+}
+mondayWork();
+
+function wrapAdjective(rose = "*") {
+    return function(str = 'speacial') {
+        return `You are ${rose}${str}${rose}!`;
+    }
+}
